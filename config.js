0a2bf164cd81f4bbd4897b5928bb118a5078e23d
4d617474 <33104312+4d617474@users.noreply.github.com>
2018-01-20 17:56:43 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,228 @@
+﻿//
+//      ███▓            ██▓ ████▓ ██▓ ████▓   ▒██ ▒████   ▒██  /git
+//     ████▓            ██▓ █▓     █▓    █▓  ▒███    ▒█  ▒███  hub.
+//    █████▓            ██▓ ████▓  █▓   █▓  ▒█████  ▒█  ▒█████ io
+//   ██████▓            ██▓ ████▓  █▓  █▓      ▒█  ▒█      ▒█  
+//  ███████▓     ██████▓██▓
+// ████████▓   ███████████▓ ▓▓▓▓ ▓▓▓▓ ▓▓▓ ▓▓▓▓ ▓▓▓▓ ▓▓  ▓ ▓▓▓▓▓ ▓▓▓▓
+//███████████▓ ███████████▓ ▓  ▓ ▓  ▓ ▓   ▓▓   ▓    ▓ ▓ ▓   ▓   ▓▓ 
+//        ██▓  ███████████▓ ▓▓▓▓ ▓▓▓  ▓▓    ▓▓ ▓▓   ▓ ▓ ▓   ▓     ▓▓
+//        ██▓   ████████▓   ▓    ▓  ▓ ▓▓▓ ▓▓▓▓ ▓▓▓▓ ▓  ▓▓   ▓   ▓▓▓▓
+// 
+//  ███████  ████████  ██    ██ ████████ ████████ ████████  ███████
+//  ██▓▓▓▓▓  ██▓▓▓▓██  ██    ██ ██▓▓▓▓██ ██▓▓▓▓██ ██▓▓▓▓▓▓  ██▓▓▓▓██
+//  ███████  ██    ██  ██    ██ ████████ ████████ ███████   ██    ██ 
+//  ▓▓▓▓▓██  ██  ████  ██    ██ ██▓▓▓▓██ ██▓▓▓██▓ ██▓▓▓▓▓   ██    ██
+//  ███████  ████████  ████████ ██    ██ ██   ███ ████████  ███████▓ 
+//  ▓▓▓▓▓▓▓  ▓▓▓▓██▓▓  ▓▓▓▓▓▓▓▓ ▓▓    ▓▓ ▓▓   ▓▓▓ ▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓  v1.0
+//
+//////////////////////////////
+// This file is used to configure the names, links and colors of the boxes, and some other stuff too 
+// There are 5 pages in total, including 40 links (8 on each pages)
+//////////////////////////////
+
+// Choose between light theme and dark theme 
+// Dark Theme = 0 / Light Theme = 1 
+
+themeNo = 0;
+
+// Choose a secondary color ( a good color picker https://color.adobe.com)
+// Example = "#A885FF" or "red" or "rgb(178,0,255)
+
+secondaryColor = "#B5FF77";
+
+////////////////////////////////////
+////////////////////////////////////
+
+var nameB = [
+
+// Here you can change the name of the boxes 
+// Example:
+// /*Box 24*/	"Buzzfeed",
+
+//-- Page 1 --//
+
+/*Box 01*/	"One",
+/*Box 02*/	"Two",
+/*Box 03*/	"Three",
+/*Box 04*/	"Four" ,
+/*Box 05*/	"Five" ,
+/*Box 06*/	"Six",
+/*Box 07*/	"Seven",
+/*Box 08*/	"Eight",
+
+//-- Page 2 --//
+
+/*Box 09*/	"Nine",
+/*Box 10*/	"Ten",
+/*Box 11*/	"Eleven",
+/*Box 12*/	"Twelve",
+/*Box 13*/	"Thirteen",
+/*Box 14*/	"Fourteen",
+/*Box 15*/	"Five",
+/*Box 16*/	"Sixteen",
+
+//-- Page 3 --//
+
+/*Box 17*/	"Seventeen",
+/*Box 18 */	"Eighteen",
+/*Box 19 */	"Nineteen",
+/*Box 20 */	"Twenty",
+/*Box 21 */	"Twenty1",
+/*Box 22 */	"Twenty2",
+/*Box 23 */	"Twenty3",
+/*Box 24 */	"Twenty4",
+
+//-- Page 4 --//
+
+/*Box 25 */	"Twenty5",
+/*Box 26 */	"Twenty6",
+/*Box 27 */	"Twenty7",
+/*Box 28 */	"Twenty8",
+/*Box 29 */	"Twenty9",
+/*Box 30 */	"Thirty",
+/*Box 31 */	"Thirty1",
+/*Box 32 */	"Thirty2",
+
+//-- Page 5 --//
+
+/*Box 33 */	"Thirty3",
+/*Box 34 */	"Thirty4",
+/*Box 35 */	"Thirty5",
+/*Box 36 */	"Thirty6",
+/*Box 37 */	"Thirty7",
+/*Box 38 */	"Thirty8",
+/*Box 39 */	"Thirty9",
+/*Box 38 */	"Forty"
+];
+
+////////////////////////////////////
+////////////////////////////////////
+
+var linkB = [
+
+// Here you can change the links of the boxes
+// Example: 
+// /*Box 13*/	"http://www.reddit.com",
+
+//-- Page 1 --//
+
+/*Box 01*/	"",
+/*Box 02*/	"",
+/*Box 03*/	"",
+/*Box 04*/	"",
+/*Box 05*/	"",
+/*Box 06*/	"",
+/*Box 07*/	"",
+/*Box 08*/	"",
+
+//-- Page 2 --//
+
+/*Box 09*/	"",
+/*Box 10*/	"",
+/*Box 11*/	"",
+/*Box 12*/	"",
+/*Box 13*/	"",
+/*Box 14*/	"",
+/*Box 15*/	"",
+/*Box 16*/	"",
+
+//-- Page 3 --//
+
+/*Box 17*/	"",
+/*Box 18 */	"",
+/*Box 19 */	"",
+/*Box 20 */	"",
+/*Box 21 */	"",
+/*Box 22 */	"",
+/*Box 23 */	"",
+/*Box 24 */	"",
+
+//-- Page 4 --//
+
+/*Box 25 */	"",
+/*Box 26 */	"",
+/*Box 27 */	"",
+/*Box 28 */	"",
+/*Box 29 */	"",
+/*Box 30 */	"",
+/*Box 31 */	"",
+/*Box 32 */	"",
+
+//-- Page 5 --//
+
+/*Box 33 */	"",
+/*Box 34 */	"",
+/*Box 35 */	"",
+/*Box 36 */	"",
+/*Box 37 */	"",
+/*Box 38 */	"",
+/*Box 39 */	"",
+/*Box 40 */	""
+];
+
+////////////////////////////////////
+////////////////////////////////////
+
+var colorB = [
+
+// Here you can change the colors of the boxes
+// Again, I recommend you do this very nice color picker >> https://color.adobe.com
+// Example:
+// /*Box 01*/	"#81C7E8",
+
+//-- Page 1 --//
+
+/*Box 01*/	"#A885FF",
+/*Box 02*/	"#E8726E",
+/*Box 03*/	"#D9C176",
+/*Box 04*/	"#6EE8E8",
+/*Box 05*/	"#B5FF77",
+/*Box 06*/	"#D98D6D",
+/*Box 07*/	"#CF74E8",
+/*Box 08*/	"#8DC8FF",
+
+//-- Page 2 --//
+
+/*Box 09*/	"#82FFE2",
+/*Box 10*/	"#7F78E8",
+/*Box 11*/	"#FF91B1",
+/*Box 12*/	"#E8BC78",
+/*Box 13*/	"#B5D970",
+/*Box 14*/	"#92FF8B",
+/*Box 15*/	"#81C7E8",
+/*Box 16*/	"#DA9AFF",
+
+//-- Page 3 --//
+
+/*Box 17*/	"#D97F76",
+/*Box 18 */	"#A77FE8",
+/*Box 19 */	"#98F7FF",
+/*Box 20 */	"#A6E87E",
+/*Box 21 */	"#FFDA89",
+/*Box 22 */	"#D9698D",
+/*Box 23 */	"#7672E8",
+/*Box 24 */	"#88FFDD",
+
+//-- Page 4 --//
+
+/*Box 25 */	"#FFFD91",
+/*Box 26 */	"#E8A978",
+/*Box 27 */	"#FF82FA",
+/*Box 28 */	"#7080D9",
+/*Box 29 */	"#8AE8BB",
+/*Box 30 */	"#AFE88B",
+/*Box 31 */	"#FFDC91",
+/*Box 32 */	"#E87978",
+
+//-- Page 5 --//
+
+/*Box 33 */	"#95D97D",
+/*Box 34 */	"#E8D298",
+/*Box 35 */	"#FFA6A0",
+/*Box 36 */	"#A586E8",
+/*Box 37 */	"#91FEFF",
+/*Box 38 */	"#D0D977",
+/*Box 39 */	"#E8BD91",
+/*Box 40 */	"#FF98E4"
+ ];
